8d595fe565880881c9f66b4a7faaa1bc2f10e3f4
yuossfalaa <yuossfalla@gmail.com>
2022-12-25 14:12:55 +0200

    INavigator Added

---
+++ Sales Monitoring/State/Navigators/INavigator.cs
@@ -0,0 +1,24 @@
+﻿using Sales_Monitoring.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sales_Monitoring.State.Navigators
+{
+    public enum ViewType
+    {
+        EditMenu,
+        HomeView,
+        RecordExpenses,
+        RecordSales,
+        ReportView
+    }
+    public interface INavigator
+    {
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
---
+++ Sales Monitoring/State/Navigators/Navigator.cs
@@ -0,0 +1,79 @@
+﻿using Sales_Monitoring.ViewModels;
+using Sales_Monitoring.Commands;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
+using System;
+
+namespace Sales_Monitoring.State.Navigators
+{
+    public class Navigator : ViewModelBase , INavigator
+    {
+        private ViewModelBase _currentviewmodel;
+        private Visibility homeviewvisibility;
+        private Visibility homebuttonvisibility;
+
+        public ICommand UpdateCurrentViewModelCommand => new Commands.UpdateCurrentViewModelCommand(this);
+        public ICommand HomeVisibleCommand { get; private set; }
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentviewmodel; }
+            set 
+            { 
+                _currentviewmodel = value; 
+                RaisePropertyChanged("CurrentViewModel");
+                if (CurrentViewModel != null) 
+                {
+                    HomeViewVisibility = Visibility.Collapsed;
+                    HomeButtonVisibility = Visibility.Visible;
+                }
+             
+            }
+        }
+
+        public Navigator()
+        {
+            HomeViewVisibility = Visibility.Visible;
+            HomeButtonVisibility = Visibility.Collapsed;
+            HomeVisibleCommand = new RelayCommand(VisbilityHandler,true);
+        }
+
+        public Visibility HomeViewVisibility
+        {
+            get
+            {
+                return homeviewvisibility;
+            }
+            set
+            {
+                homeviewvisibility = value;
+
+                RaisePropertyChanged("HomeViewVisibility");
+            }
+        }
+        public Visibility HomeButtonVisibility
+        {
+            get
+            {
+                return homebuttonvisibility;
+            }
+            set
+            {
+                homebuttonvisibility = value;
+
+                RaisePropertyChanged("HomeButtonVisibility");
+            }
+        }
+
+
+        private void VisbilityHandler()
+        {
+            HomeViewVisibility = Visibility.Visible;
+            HomeButtonVisibility = Visibility.Collapsed;
+            CurrentViewModel = null;
+        }
+
+    }
+}
