ba89d086b506daba94b38701ba4ffcf15b276c19
yuossfalaa <yuossfalla@gmail.com>
2022-12-25 15:53:20 +0200

    Add Expenses Model

---
+++ Sales Monitoring/SalesMonitoring.Domain/Models/RecordExpenses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales_Monitoring.SalesMonitoring.Domain.Models
+{
+    public class RecordExpenses : DomainObject
+    {
+        public DateTime Date { get; set; }
+        public string ItemName { get; set; }
+        public float Amount { get; set; }
+        public string Mode { get; set; }
+    }
+}
