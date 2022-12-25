4a2b8fd0755df8c54e4bf2a205af9d1270157f24
yuossfalaa <yuossfalla@gmail.com>
2022-12-25 15:53:37 +0200

    Add IDataService

---
+++ Sales Monitoring/SalesMonitoring.Domain/Services/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sales_Monitoring.SalesMonitoring.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(int id, T entity);
+        Task<bool> Delete(T entity);
+
+    }
+}
