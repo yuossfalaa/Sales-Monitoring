827db1904db882302cffa99ff069d0926cb2d3aa
yuossfalaa <yuossfalla@gmail.com>
2022-12-25 15:53:02 +0200

    Add item Model

---
+++ Sales Monitoring/SalesMonitoring.Domain/Models/Items.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales_Monitoring.SalesMonitoring.Domain.Models
+{
+    public class Items : DomainObject
+    {
+        
+        public string? ItemName { get; set; }
+        public float? ItemPrice { get; set; }
+        public float? ItemInstorePrice { get; set; }
+        public float? ItemZomatoPrice { get; set; }
+        public float? ItemSwiggyPrice { get; set; }
+
+    }
+}
