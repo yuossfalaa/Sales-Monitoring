c2ec8562b9e1545af78899a8c1298247ad461ca2
yuossfalaa <yuossfalla@gmail.com>
2022-12-25 15:53:09 +0200

    Add order Model

---
+++ Sales Monitoring/SalesMonitoring.Domain/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace Sales_Monitoring.SalesMonitoring.Domain.Models
+{
+    public class Order : DomainObject
+    {
+        private Items _item { get; set; }
+        public Items item
+        {
+            get { return _item; }
+            set { _item = value; RaisePropertyChanged("item"); }
+        }
+
+        private int _quantity { get; set; }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set 
+            {
+                 _quantity = value;
+                RaisePropertyChanged("Quantity");
+            }
+        }
+
+        #region RaisePropertyChanged
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+
+    }
+}
