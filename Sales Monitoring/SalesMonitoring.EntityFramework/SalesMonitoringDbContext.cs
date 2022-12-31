f6fe98596db08cb67baf43c4a2151a0ae64e7cb4
yuossfalaa <yuossfalla@gmail.com>
2022-12-31 14:18:29 +0200

    Adding Db Context of EF

---
+++ Sales Monitoring/SalesMonitoring.EntityFramework/SalesMonitoringDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales_Monitoring.SalesMonitoring.Domain.Models;
+
+namespace Sales_Monitoring.SalesMonitoring.EntityFramework
+{
+    public class SalesMonitoringDbContext : DbContext
+    {
+
+        public DbSet<Items> Items { get;set;}
+        public DbSet<ItemSales> ItemSales { get;set;}
+        public DbSet<Order> Orders { get;set;}
+        public DbSet<RecordExpenses> RecordExpenses { get;set;}
+        public SalesMonitoringDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Sales Monitoring/SalesMonitoring.EntityFramework/SalesMonitoringDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Sales_Monitoring.SalesMonitoring.EntityFramework
+{
+    public class SalesMonitoringDbContextFactory : IDesignTimeDbContextFactory<SalesMonitoringDbContext>
+    {
+        public SalesMonitoringDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<SalesMonitoringDbContext>();
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SalesMonitoringDb;Trusted_Connection=True;");
+            return new SalesMonitoringDbContext(options.Options);
+
+        }
+    }
+}
