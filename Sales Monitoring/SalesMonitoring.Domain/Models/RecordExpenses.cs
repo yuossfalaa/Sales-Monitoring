9d2e1d1aa17153af1ec8fbc07c46a7198bfc3772
yuossfalaa <yuossfalla@gmail.com>
2022-12-31 21:14:42 +0200

    .

--- Sales Monitoring/SalesMonitoring.Domain/Models/RecordExpenses.cs
+++ Sales Monitoring/SalesMonitoring.Domain/Models/RecordExpenses.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Sales_Monitoring.SalesMonitoring.Domain.Models
 {
     public class RecordExpenses : DomainObject
     {
-        public DateTime Date { get; set; }
-        public string ItemName { get; set; }
-        public double Amount { get; set; }
-        public string Mode { get; set; }
+        private DateTime _date;
+        private string _itemname;
+        private string _mode;
+        private double _amount;
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; RaisePropertyChanged("Date"); }
+        }
+        public string ItemName 
+        {
+            get { return _itemname; }
+            set { _itemname = value; RaisePropertyChanged("ItemName"); }
+        }
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; RaisePropertyChanged("Amount"); }
+        }
+        public string Mode
+        {
+            get { return _mode; }
+            set { _mode = value; RaisePropertyChanged("Mode"); }
+        }
+
+        #region RaisePropertyChanged
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
     }
 }
