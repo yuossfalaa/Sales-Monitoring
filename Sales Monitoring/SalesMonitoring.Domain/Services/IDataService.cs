fb277a213b7e0eb50569fc0adf1498d216718282
yuossfalaa <yuossfalla@gmail.com>
2022-12-31 14:17:50 +0200

    Adding Data Service InterFace

--- Sales Monitoring/SalesMonitoring.Domain/Services/IDataService.cs
+++ Sales Monitoring/SalesMonitoring.Domain/Services/IDataService.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using Sales_Monitoring.SalesMonitoring.Domain.Models;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Sales_Monitoring.SalesMonitoring.Domain.Services
 {
     public interface IDataService<T>
     {
-        Task<IEnumerable<T>> GetAll();
+        Task<List<T>> GetAll();
+        Task<List<Order>> GetAllOrdersBetweenDates(DateTime Start, DateTime End);
         Task<T> Get(int id);
         Task<T> Create(T entity);
         Task<T> Update(int id, T entity);
