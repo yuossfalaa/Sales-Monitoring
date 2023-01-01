ef8476404d73b5612f9870fd3f90a13a13e84dd5
yuossfalaa <yuossfalla@gmail.com>
2023-01-01 22:53:30 +0200

    New Domainobj

--- Sales Monitoring/SalesMonitoring.Domain/Models/DomainObject.cs
+++ Sales Monitoring/SalesMonitoring.Domain/Models/DomainObject.cs
@@ -1,6 +1,8 @@
-﻿namespace Sales_Monitoring.SalesMonitoring.Domain.Models
+﻿using GalaSoft.MvvmLight;
+
+namespace Sales_Monitoring.SalesMonitoring.Domain.Models
 {
-    public class DomainObject
+    public class DomainObject : ViewModelBase
     {
         public int Id { get; set; }
     }
