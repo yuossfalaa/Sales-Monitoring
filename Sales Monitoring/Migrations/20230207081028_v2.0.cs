bd269cd64d2da958ba45ce1431e6d3a082c56b69
yuossfalaa <yuossfalla@gmail.com>
2023-02-07 10:52:50 +0200

    New DB

---
+++ Sales Monitoring/Migrations/20230207081028_v2.0.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesMonitoring.Migrations
+{
+    /// <inheritdoc />
+    public partial class v20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxId",
+                table: "RecordExpenses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "VendorName",
+                table: "RecordExpenses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxId",
+                table: "RecordExpenses");
+
+            migrationBuilder.DropColumn(
+                name: "VendorName",
+                table: "RecordExpenses");
+        }
+    }
+}
