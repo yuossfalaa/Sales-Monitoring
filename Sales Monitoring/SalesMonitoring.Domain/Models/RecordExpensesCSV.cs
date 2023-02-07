d9d76cc39280b2c0cf3b603567a996f5b1ef8da2
yuossfalaa <yuossfalla@gmail.com>
2023-02-07 10:53:09 +0200

    Fix Date Format

---
+++ Sales Monitoring/SalesMonitoring.Domain/Models/RecordExpensesCSV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales_Monitoring.SalesMonitoring.Domain.Models
+{
+    public class RecordExpensesCSV
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string ItemName { get; set; }
+        public string VendorName { get; set; }
+        public string TaxId { get; set; }
+        public double Amount { get; set; }
+        public string Mode { get; set; }
+    }
+}
